cff976a56fc6f095ae998251f67543969329b2f2
Nazim Lachter <lachtater@gmail.com>
2017-10-27 13:56:07 +0200

    Issues : create, change

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 
 const utf8 = require('utf8');
 const hmacsha1 = require('hmacsha1');
+const githubApi = require('github-api');
 
 
 /********************************************************
@@ -32,8 +33,6 @@ const TAIGA_SECRET = process.env.TAIGA_SECRET;
 
 const fs = require('fs');
 
-const PATHS = { issues: 'data/issues.json', users: 'data/users.json' };
-
 function updateFile(path, jsonContent) {
 	fs.writeFile(path, JSON.stringify(jsonContent), 'utf8', function(err) {
     	if(err) console.log("ERROR :", err);
@@ -49,38 +48,62 @@ function readFile(path, callback) {
 
 let DATA = { issues: {}, users: {} };
 
-// Issues
-if(fs.existsSync(PATHS.issues)) {
-	readFile(PATHS.issues, issues => { DATA.issues = issues; });
-} else {
-	updateFile(PATHS.issues, DATA.issues);
+function loadData(identifiers) {
+	for(let i = 0; i < identifiers.length; i++) {	
+		if(fs.existsSync("data/" + identifiers[i] + ".json")) {
+			readFile("data/" + identifiers[i] + ".json", content => { DATA[identifiers[i]] = content; });
+		} else {
+			updateFile("data/" + identifiers[i] + ".json", DATA[identifiers[i]]);
+		}
+	}
 }
 
-// Users
-if(fs.existsSync(PATHS.users)) {
-	readFile(PATHS.users, users => { DATA.users = users; });
-} else {
-	updateFile(PATHS.users, DATA.users);
-}
+loadData([
+	'issues', 	// List of Taiga to GitHub issues ids
+	'projects',	// List of Taiga projects to GitHub repository
+	'users',	// List of Taiga to GitHub users ids
+]);
 
 
 /********************************************************
   *   GITHUB
   ********************************************************/
 
-const GitHub = require('github-api')({ token: process.env.GITHUB_TOKEN });
+const GitHub = new githubApi({ token: process.env.GITHUB_TOKEN });
 
-function saveGitHubIssue(taigaId, gitHubId) {
+/*function saveGitHubIssue(taigaId, gitHubId) {
 	DATA.issues[taigaId] = gitHubId;
 	updateFile(PATHS.issues, DATA.issues);
 }
 
 function getGitHubIssue(taigaId) {
 	console.log(DATA.issues[id]);
-}
+}*/
 
-function createIssue(data) {
-  	//GitHub.createIssue({});
+// ISSUES
+const GitHubIssues = GitHub.getIssues(process.env.GITHUB_OWNER, process.env.GITHUB_REPO);
+
+const handle = (promise, successMessage) => {
+	promise.then(output => {
+		//console.log(output);
+		console.log(successMessage);
+	}).catch(error => {
+		console.log("ERROR :", error);
+	});
+};
+
+function updateIssue(taigaId, author, subject, description, assignment) {
+	console.log(taigaId, author, subject, description, assignment);
+	const data = {
+		title: '[' + taigaId + '] ' + subject,
+		body: description
+	};
+	if(typeof DATA.users[assignment] !== 'undefined') data.assignees = [ DATA.users[assignment] ];
+	if(typeof DATA.issues[taigaId] === 'undefined') {
+		handle(GitHubIssues.createIssue(data), "Issue for " + taigaId + " created");
+	} else {
+		handle(GitHubIssues.editIssue(DATA.issues[taigaId], data), "Issue for " + taigaId + " updated");
+	}
 }
 
 
@@ -89,18 +112,39 @@ function createIssue(data) {
   ********************************************************/
 
 app.post('/taiga', (req, res) => {
-	const data = req.body;
+	const webhook = req.body;
 	//const signature = req.headers['x-taiga-webhook-signature'];
-    //console.log(utf8.encode(data));
-  	console.log(data);
+    //console.log(utf8.encode(webhook));
+  	console.log(webhook);
 
-	switch(data.action) {
-	    case 'issues':
-	    	console.log(data);
-	        break;
-	    default:
-	        console.log("ERROR : Unknown action \"" + data.action + "\"");
-	}
+  	if(DATA.projects.indexOf(webhook.data.project.id) === -1) {
+  		console.log("ERROR : Project \"" + webhook.data.project.id + "\" not in the list");
+  	} else {
+		switch(webhook.type) {
+
+			// ISSUES
+		    case 'issue':
+		    	switch(webhook.action) {
+		    		case 'create':
+		    		case 'change':
+		    			updateIssue(
+		    				webhook.data.ref,			// taigaId
+		    				webhook.by.username,		// author
+		    				webhook.data.subject,		// subject
+		    				webhook.data.description,	// description
+		    				webhook.data.assigned_to,	// assignment
+		    			);
+		    			break;
+		    		default:
+		    			console.log("ERROR : Unknown action \"" + webhook.action + "\"");
+		    	}
+		        break;
+
+	        // NONE
+		    default:
+		        console.log("ERROR : Unknown type \"" + webhook.type + "\"");
+		}
+  	}
   
   	res.end("OK");
 });
